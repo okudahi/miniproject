e6f80504bda69cf8d8137d7d5e657472454de216
okudahi <pabrika@mineo.jp>
2022-06-27 02:10:53 +0900

    small change

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-start_time=`date +%m-%d %H:%M:%S`
+start_time=`date "+%m/%d %H:%M:%S"`
 #PREFIX_SIZES=(0 80 640)
 #PREFIX_SIZES=(0 10 20 40 80 160 320 640)
 PREFIX_SIZES=(0 5 10 20 30 40 50 60 70 80)
@@ -12,6 +12,9 @@ exec_test() {
 }
 echo $start_time
 exec_test
-end_time=`date +%m-%d %H:%M:%S`
-echo $start_time
-echo $end_time
+end_time=`date "+%m/%d %H:%M:%S"`
+echo start: $start_time
+echo end: $end_time
+diff=$(expr `date -d"$end_time" +%s` - `date -d"$start_time" +%s`)
+echo "$diff"秒
+echo `expr "$diff" / 60`分
