77504f0a591663533f8272b7c2498f4da449897d
okudahi <pabrika@mieno.jp>
2022-07-20 14:46:34 +0900

    add masstree_pseudocode

--- Test.java
+++ Test.java
@@ -3,13 +3,9 @@
 public class Test {
     public static void main(String[] args){
         
-        String a = " ";
-        String b = "";
-        String c = "zzzzzzzz";
-        String d = "~";
-        System.out.println(d.compareTo(c));
-        System.out.println(c.compareTo(b));
-        System.out.println(b.compareTo(a));
+        int a = 5;
+        int b = 10;
+        System.out.println(a^b);
         return;
     }
 }
